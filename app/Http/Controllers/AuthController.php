79b0b9de9be57eba9d9dcc4b12532b806dec53cb
ndkvin <andika.kavin@gmail.com>
2023-07-09 20:25:08 +0700

    Init repo

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\AccessToken;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+
+class AuthController extends Controller
+{
+  public function register(Request $request)
+  {
+    $validate = Validator::make($request->all(), [
+      'name' => 'required|min:3|max:55',
+      'email' => 'email|required|unique:users',
+      'password' => 'required|min:8',
+      'phone' => 'required|min:10|max:12'
+    ]);
+
+    if ($validate->fails()) {
+      $errors = $validate->errors();
+      $errorMessage = [];
+
+      foreach ($errors->all() as $message) {
+        $errorMessage[] = $message;
+      }
+
+      return response()->json([
+        'success' => false,
+        'code' => 422,
+        'errors' => $errorMessage
+      ], 422);
+    }
+
+    $data = $request->all();
+    $data['password'] = bcrypt($data['password']);
+
+    $user = User::create($data);
+
+    $accessToken = $user->createToken('authToken')->accessToken;
+
+    return response([
+      'success' => true,
+      'code' => 200,
+      'data' => [
+        'user' => $user,
+        'access_token' => $accessToken
+      ]
+    ]);
+  }
+
+  public function login(Request $request)
+  {
+    $validate = Validator::make($request->all(), [
+      'email' => 'email|required',
+      'password' => 'required|min:8'
+    ]);
+
+    if ($validate->fails()) {
+      $errors = $validate->errors();
+      $errorMessage = [];
+
+      foreach ($errors->all() as $message) {
+        $errorMessage[] = $message;
+      }
+
+      return response()->json([
+        'success' => false,
+        'code' => 422,
+        'errors' => $errorMessage
+      ], 422);
+    }
+
+    if (!auth()->attempt($request->all())) {
+      return response()->json([
+        'success' => false,
+        'code' => 400,
+        'message' => ['credential not match to our record']
+      ], 400);
+    }
+
+    $accessToken = auth()->user()->createToken('authToken')->accessToken;
+
+    return response(
+      [
+        'success' => true,
+        'code' => 200,
+        'data' => [
+          'user' => auth()->user(),
+          'access_token' => $accessToken
+        ]
+      ]
+    );
+  }
+
+  public function profile(Request $request)
+  {
+    $user = Auth::guard('api')->user();
+
+    if ($user) {
+      return response()->json([
+        'success' => true,
+        'code' => 200,
+        'data' => $user
+      ]);
+    }
+  }
+}
---
+++ routes/api.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Models\User;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::post('/register', [App\Http\Controllers\AuthController::class, 'register']);
+Route::post('/login', [App\Http\Controllers\AuthController::class, 'login']);
+Route::post('/profile', [App\Http\Controllers\AuthController::class, 'profile'])->middleware('auth:api');
